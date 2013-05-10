e226887133941e87868593d570baf803783de884
Som Poddar <som.poddar@gmail.com>
2013-05-09 23:46:26 -0400

    re-formatted solution structure.
    + Added controller and base classes

---
+++ MVC-FormWizard/MVC-FormWizard/Controllers/WizardController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_FormWizard.Controllers
+{
+    public class WizardController : Controller
+    {
+        //
+        // GET: /Wizard/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult StepOne()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public ActionResult StepOne()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult StepTwo()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public ActionResult StepTwo()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult StepThree()
+        {
+            return View();
+        }
+
+
+        //[HttpPost]
+        //public ActionResult StepThree()
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ MVC-FormWizard/MVC-FormWizard/Models/StepOne.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_FormWizard.Models
+{
+    public class StepOne
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
